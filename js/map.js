ab24378a3ae67725d920e595cd357af4417e2332
ElisabethCheston <elisabeth.cheston@gmail.com>
2020-12-15 15:38:30 +0000

    add windy map hellow world.

--- js/map.js
+++ js/map.js
@@ -1,5 +1,33 @@
 
+const options = {
+    key: '2HKms1Uf1Q6WEcVvlflRiW1nxBlyVjdN', // REPLACE WITH YOUR KEY !!!
 
+    // Put additional console output
+    verbose: true,
+
+    // Optional: Initial state of the map
+    lat: 62.45,
+    lon: 17.52,
+    zoom: 5,
+};
+
+// Initialize Windy API
+windyInit(options, windyAPI => {
+    // windyAPI is ready, and contain 'map', 'store',
+    // 'picker' and other usefull stuff
+
+    const { map } = windyAPI;
+    // .map is instance of Leaflet map
+
+    L.popup()
+        .setLatLng([50.4, 14.3])
+        .setContent('Hello World')
+        .openOn(map);
+});
+
+
+
+/*
 //  Array of markers //
     var markers = [
         //Kalix//
